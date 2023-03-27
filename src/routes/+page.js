d59e78a45136ba4014ebaae9fad7094a0fdb4a97
Duy Nguyen <dunguyen@adobe.com>
2023-03-27 17:10:27 +0200

    do not prerender in author mode

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,3 +1,5 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-export const prerender = true;
+import { PUBLIC_AEM_HOST } from '$env/static/public';
+
+export const prerender = PUBLIC_AEM_HOST.includes('author') ? false : true;
